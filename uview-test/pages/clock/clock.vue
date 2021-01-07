5668415674502c45affc3667bb8e57854d6afa1f
qiunanya <l>
2021-01-07 22:26:00 +0800

    增加底部菜单栏和图标

---
+++ uview-test/pages/clock/clock.vue
@@ -0,0 +1,28 @@
+<template>
+	<view class="mine_container">
+		{{key}}
+	</view>
+</template>
+
+<script>
+	export default{
+		data() {
+			return {
+				key: "我的"
+			}
+		},
+		methods: {
+			name() {
+				
+			}
+		},
+		mounted() {
+			
+		}
+	}
+</script>
+<style lang="scss">
+	.mine_container{
+		
+	}
+</style>
